c5b486f11bd88bc8fa044db7a83484f7b0f80388
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-02-08 16:27:33 +0100

    Update Queue.py
    - added enqueue and dequeue function
    - added iteration functionality

--- Queue.py
+++ Queue.py
@@ -6,14 +6,24 @@ class Queue:
         self.__tail = None
         self.__size = 0
 
-
     def __len__(self):
         return self.__size
 
     def empty(self):
         return self.__head is None and self.__tail is None
+    
+    def __iter__(self):
+        self.__current = self.__head
+        return self
+    
+    def __next__(self):
+        if self.__current is None:
+            raise StopIteration()
+        result = self.__current
+        self.__current = self.__current.next
+        return result
 
-    def push(self, item):
+    def enqueue(self, item):
         if item is not None:
             if self.__head is None:
                 self.__head = item
@@ -27,6 +37,15 @@ class Queue:
                 item = self.__tail
 
             self.__size += 1
+    
+    def dequeue(self):
+        if self.empty():
+            return None
+        else:
+            node = self.__head
+            nextnode = self.__head.next
+            self.__head = nextnode
+            return node
 
 if __name__ == "__main__":
     pass
