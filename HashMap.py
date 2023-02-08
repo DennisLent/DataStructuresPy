817ce2652c9760fab128b43834f7831de7f0f275
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-02-08 15:28:02 +0100

    Update HashMap.py
    fixed remove function and changed functionality so that hashfunction can be changed

--- HashMap.py
+++ HashMap.py
@@ -2,9 +2,10 @@ from Nodes import Node2
 
 class HashMap:
 
-    def __init__(self, size):
+    def __init__(self, size, hashfunc):
         self.__size = size
         self.__buckets = [None for _ in range(self.__size + 1)]
+        self.__hashfunc = hashfunc
 
     def __str__(self):
         return_string  = "| "
@@ -19,15 +20,9 @@ class HashMap:
             return_string += "]"
         return return_string + " |"
     
-    def hashfunc(self, key):
-        index = 0
-        for letter in key:
-            index += ord(letter)
-        return (index % self.__size)
-    
     def insert_data(self, node):
         if node is not None:
-            index = self.hashfunc(node.key)
+            index = self.__hashfunc(node.key, self.__size)
             if self.__buckets[index] is None:
                 self.__buckets[index] = node
             else:
@@ -39,7 +34,7 @@ class HashMap:
     
     def get_data(self, key):
         if key is not None:
-            index = self.hashfunc(key)
+            index = self.__hashfunc(key, self.__size)
             iternode = self.__buckets[index]
             while iternode is not None:
                 if key == iternode.key:
@@ -49,7 +44,7 @@ class HashMap:
     
     def remove_data(self, key):
         if key is not None:
-            index = self.hashfunc(key)
+            index = self.__hashfunc(key, self.__size)
             iternode = self.__buckets[index]
             if iternode.key == key:
                 if iternode.next is not None:
@@ -57,6 +52,7 @@ class HashMap:
                     self.__buckets[index].prev = None
                 else:
                     self.__buckets[index] = None
+                return
             
             while iternode is not None:
                 if key == iternode.key:
@@ -64,24 +60,21 @@ class HashMap:
                     prevnode = iternode.prev
                     prevnode.next = nextnode
                     nextnode.prev = prevnode
+                    return
                 iternode = iternode.next
         return f"key: {key} not found"
 
                 
 
-
-
-
-
-            
-
-                    
-    
-
-
 if __name__ == "__main__":
 
-    hm = HashMap(3)
+    def hashfunc(key, size):
+        index = 0
+        for letter in key:
+            index += ord(letter)
+        return (index % size)
+
+    hm = HashMap(3, hashfunc)
     print(hm)
 
     node_lst = [Node2(1, "ab"), Node2(2, "gh"), Node2(3, "zz"), Node2(4, "qq"), Node2(5, "hjq"), Node2(6, "ggwp"), Node2(7, "car"), Node2(8, "ass"), Node2(9, "chill"), Node2(10, "cheese")]
