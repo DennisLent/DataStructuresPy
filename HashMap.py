990f59775e827e7eb0b47551cac2fe09cf5da9a4
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-02-08 13:02:26 +0100

    Update HashMap.py
    working on removing method

--- HashMap.py
+++ HashMap.py
@@ -6,11 +6,10 @@ class HashMap:
         self.__size = size
         self.__buckets = [None for _ in range(self.__size + 1)]
 
-    
     def __str__(self):
-        return_string  = "|"
+        return_string  = "| "
         for node in self.__buckets:
-            return_string += " ["
+            return_string += "["
             if node is not None:
                 while node is not None:
                     return_string += f"{node} "
@@ -18,7 +17,7 @@ class HashMap:
             else:
                 return_string += "*"
             return_string += "]"
-        return return_string + "|"
+        return return_string + " |"
     
     def hashfunc(self, key):
         index = 0
@@ -46,7 +45,35 @@ class HashMap:
                 if key == iternode.key:
                     return iternode.data
                 iternode = iternode.next
-            return f"{key} not found"
+        return f"key: {key} not found"
+    
+    def remove_data(self, key):
+        if key is not None:
+            index = self.hashfunc(key)
+            iternode = self.__buckets[index]
+            if iternode.key == key:
+                if iternode.next is not None:
+                    self.__buckets[index] = iternode.next
+                    self.__buckets[index].prev = None
+                else:
+                    self.__buckets[index] = None
+            
+            while iternode is not None:
+                if key == iternode.key:
+                    nextnode = iternode.next
+                    prevnode = iternode.prev
+                    prevnode.next = nextnode
+                    nextnode.prev = prevnode
+                iternode = iternode.next
+        return f"key: {key} not found"
+
+                
+
+
+
+
+
+            
 
                     
     
@@ -57,7 +84,7 @@ if __name__ == "__main__":
     hm = HashMap(3)
     print(hm)
 
-    node_lst = [Node2(1, "ab"), Node2(2, "gh"), Node2(3, "zz"), Node2(4, "qq"), Node2(5, "hjq"), Node2(6, "ggwp")]
+    node_lst = [Node2(1, "ab"), Node2(2, "gh"), Node2(3, "zz"), Node2(4, "qq"), Node2(5, "hjq"), Node2(6, "ggwp"), Node2(7, "car"), Node2(8, "ass"), Node2(9, "chill"), Node2(10, "cheese")]
     for node in node_lst:
         hm.insert_data(node)
     
@@ -65,6 +92,12 @@ if __name__ == "__main__":
 
     print(hm.get_data("ab"))
     print(hm.get_data("abc"))
+
+    hm.remove_data("gh")
+    hm.remove_data("zz")
+    hm.remove_data("dennis")
+    print(hm)
+
     
 
     
