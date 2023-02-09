cced1da0e89a62adfec48a5cf6db570ce3a4e55b
DennisLent <62008839+DennisLent@users.noreply.github.com>
2023-02-09 14:14:41 +0100

    Added BinaryTree.py updated Nodes.py
    - Added script for BinaryTree
    - Added BSTNode class for BinaryTree

--- Nodes.py
+++ Nodes.py
@@ -47,7 +47,6 @@ class InvalidKeyException(Exception):
     "Raised when key is not a string"
     pass
 
-
 class Node2:
     """
     Basic Node structure for Hashmap
@@ -98,6 +97,51 @@ class Node2:
     data = property(get_data, set_data)
 
     key = property(get_key, set_key)
+
+    def __lt__(self, other):
+        return self.data < other.data
+
+    def __gt__(self, other):
+        return self.data > other.data
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+class BSTNode:
+    """
+    Basic Node structure for binary tree
+    """
+    def __init__(self, data, left=None, right=None):
+        self.__data = data
+        self.__left = left
+        self.__right = right
+
+    def __str__(self):
+        return f"{self.__data}"
+
+    def get_right(self):
+        return self.__right
+
+    def set_right(self, item):
+        self.__right = item
+
+    def get_left(self):
+        return self.__left
+
+    def set_left(self, item):
+        self.__left = item
+
+    def get_data(self):
+        return self.__data
+
+    def set_data(self, item):
+        self.__data = item
+
+    right = property(get_right, set_right)
+
+    left = property(get_left, set_left)
+
+    data = property(get_data, set_data)
 
     def __lt__(self, other):
         return self.data < other.data
